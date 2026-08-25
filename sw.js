f377ae8c4245afa46c2d8037680688a1ff3dc04c
Deneb Pulsipher <deneb@seamonsterstudios.com>
2026-08-24 18:03:49 -0600

    adjust mobile styles so buttons fit with new convo type selector

--- sw.js
+++ sw.js
@@ -1,3 +1,12 @@
 self.addEventListener('install', (event) => {
   self.skipWaiting();
 });
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  // Silence the error by allowing the fetch to proceed natively
+  event.respondWith(fetch(event.request));
+});
