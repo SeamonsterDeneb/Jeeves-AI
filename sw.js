78cfae11937cfe7762fec9d6c250581bc81ace8f
Deneb Pulsipher <deneb@seamonsterstudios.com>
2026-09-11 18:34:07 -0600

    perfect service worker so can run without host, but still grabs new code when possible

--- sw.js
+++ sw.js
@@ -1,12 +1,21 @@
+const CACHE_NAME = 'v1';
+const ASSETS = ['/', '/index.html', '/css/styles.css', '/js/jeeves.js'];
+
 self.addEventListener('install', (event) => {
   self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(clients.claim());
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+    )).then(() => clients.claim())
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-  // Silence the error by allowing the fetch to proceed natively
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
 });
