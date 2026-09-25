b12d10c0fde3c61713231f676594dd96bee39497
Deneb Pulsipher <deneb@seamonsterstudios.com>
2026-09-25 04:33:36 -0600

    microadjust trigger phrases

--- js/jeeves.js
+++ js/jeeves.js
@@ -1946,7 +1946,7 @@
       }
     };
 
-    const TRIGGER_REGEX = /\b(?:what do you think|your thoughts|over to you|take it away|if you please|thank you),?\s*(?:jeeves|chief|geeves|jeevs|jeans|teams)[\s.,!?]*$/i;
+    const TRIGGER_REGEX = /\b(?:what do you think|what do you say|your thoughts|over to you|take it away|if you please|thank you),?\s*(?:jeeves|chief|geeves|jeevs|jeans|teams)[\s.,!?]*$/i;
 
     recognition.onresult = (event) => {
       liveInterim = '';
